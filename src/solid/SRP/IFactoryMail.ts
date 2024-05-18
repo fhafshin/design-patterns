997fc58dc2bc062502044b9a58a4dfb13ad214ae
fhafshin <fhafshin@gmail.com>
2024-05-18 16:11:10 +0330

    SRP

--- src/solid/SRP/IFactoryMail.ts
+++ src/solid/SRP/IFactoryMail.ts
@@ -5,7 +5,5 @@ export default interface IFactoryMail {
   ITranslateMail: ITranslateMail;
   ITempleteMail: ITempleteMail;
 
-  constructor(ITranslateMail: ITranslateMail, ITempleteMail: ITempleteMail): {};
-
   getMessage(): string;
 }
