637c51958b14810c5b429aae968f91220aeea45b
fhafshin <fhafshin@gmail.com>
2024-06-16 10:25:46 +0330

    finall

--- src/solid/ISP/IranSMSProvider.ts
+++ src/solid/ISP/IranSMSProvider.ts
@@ -1,12 +1,8 @@
 import Notifier from "./Notifier";
-<<<<<<< HEAD
 
-export default class IranSMSProvider implements Notifier {
-=======
 import SMSProvider from "./SMSProvider";
 
 export default class IranSMSProvider implements SMSProvider {
->>>>>>> af6f3d6fa9b8eb5fa94d1cb5ee6ec39fbf0dd46d
   sendSMS(message: string): void {
     throw new Error("Method not implemented.");
   }
--- src/solid/ISP/Pusher.ts
+++ src/solid/ISP/Pusher.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import Notifier from "./Notifier";
 
 export default class Pusher implements Notifier {
-=======
-import PushProvider from "./PushProvider";
-
-export default class Pusher implements PushProvider {
->>>>>>> af6f3d6fa9b8eb5fa94d1cb5ee6ec39fbf0dd46d
   sendSMS(message: string): void {
     throw new Error("Method not implemented.");
   }
