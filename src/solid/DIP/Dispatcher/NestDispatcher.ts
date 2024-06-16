85d1f4cf0de1698446e4f2653ab337be023cf6fb
fhafshin <fhafshin@gmail.com>
2024-06-16 14:16:53 +0330

    finall v2

--- src/solid/DIP/Dispatcher/NestDispatcher.ts
+++ src/solid/DIP/Dispatcher/NestDispatcher.ts
@@ -1,4 +1,6 @@
-export default class NestDispatcher {
+import IDispatcher from "./IDispatcher";
+
+export default class NestDispatcher implements IDispatcher {
   public dispatch(event: string, payload: Object): void {
     console.log("dispatch....");
   }
